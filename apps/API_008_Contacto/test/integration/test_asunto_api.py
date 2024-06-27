da52e1f40f69cf4aac7e4381d22b22f604b55ac1
Gustavo <gustavocartes05@gmail.com>
2024-06-27 14:46:37 -0400

    Test integration Asunto

--- apps/API_008_Contacto/test/integration/test_asunto_api.py
+++ apps/API_008_Contacto/test/integration/test_asunto_api.py
@@ -22,10 +22,40 @@ class TestAsuntoAPI:
 
         assert response.status_code == status.HTTP_201_CREATED
     
-    def test_get_asunt_no_existente(self):
+    def test_get_asunto_no_existente(self):
         """
         Se debe probar el error al hacer un get hacia un asunto_id no existente
         """
         client = APIClient()
-        url = reverse('contacto-detail')
+        url = reverse('contacto-detail',kwargs={'pk': 200})
+
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+    def test_asunto_crear_sin_datos(self):
+        """
+        Se debe validar de que no se cree el asunto si no tene texto
+        """
+        client = APIClient()
+        url = reverse('asunto-create')
+
+        data = {
+            'nombre': ''
+        }
+
+        response = client.post(url, data, follow='json')
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_asunto_eliminar_no_exustente(self):
+        """
+        Se debe validar el error controlado al intentar eliminar un asunto_id que no existe
+        """
+        client = APIClient()
+        url = reverse('contacto-detail', kwargs={'pk': 200})
+
+        response = client.delete(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
         
